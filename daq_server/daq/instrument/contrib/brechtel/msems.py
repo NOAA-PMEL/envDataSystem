035af40179bfa58e343309fe307686d1fe0169d1
Derek Coffman <derek.coffman@noaa.gov>
2020-07-21 00:48:44 -0700

    debug

--- daq_server/daq/instrument/contrib/brechtel/msems.py
+++ daq_server/daq/instrument/contrib/brechtel/msems.py
@@ -254,10 +254,10 @@ class MSEMS(BrechtelInstrument):
                     flow = self.get_data_record_param(dt, 'mcpc_sample_flow')
                     bin_time = self.get_data_record_param(dt, 'bin_time')
                     print(f'flow: {flow}, bin_time: {bin_time}')
-                    if flow is None:
-                        flow = 0.35
-                    if bin_time is None:
-                        bin_time = 1
+                    # if flow is None:
+                    #     flow = 0.35
+                    # if bin_time is None:
+                    #     bin_time = 1
                     print(f'here: {type(flow)}, {type(bin_time)}')
                     cm3 = flow*1000./60./bin_time
                     print(f'flow: {flow}, bin_time: {bin_time}, cm3: {cm3}')
