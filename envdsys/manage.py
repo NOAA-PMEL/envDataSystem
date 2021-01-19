eb29cb393693859cdafde7c97b194cf30f2eed44
Derek Coffman <derek.coffman@noaa.gov>
2021-01-19 22:58:43 +0000

    reorganize config and db folders

--- envdsys/manage.py
+++ envdsys/manage.py
@@ -3,7 +3,8 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'envdsys.settings')
+    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'envdsys.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.envdsys.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
