a855e6c4bbdb64883f32268aafc289848093bb5c
Derek Coffman <derek.coffman@noaa.gov>
2021-01-26 22:06:44 +0000

    reso

---
+++ daq_server/config/daq_settings_orig.py
@@ -0,0 +1,17 @@
+
+server_config = {
+
+    'name': 'Test Server',
+
+    # 'ui_config': {
+    #     'host': '192.168.86.51',
+    #     'port': 8001
+    # },
+    'ui_config': {
+        'host': 'localhost',
+        'port': 8001
+    },
+
+    'base_file_path': '/home/horton/derek/Data/envDataSystem'
+
+}
