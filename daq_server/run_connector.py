d854f42ff7ef05e61e7c685fcb08267981b3f180
Derek Coffman <derek.coffman@noaa.gov>
2021-01-29 09:49:39 -0800

    test new setup and format with black

--- daq_server/run_connector.py
+++ daq_server/run_connector.py
@@ -5,47 +5,46 @@ from connector.connector import WSConnectorServer, WSConnectorUI
 
 async def heartbeat():
     while True:
-        print('lub-dub')
+        print("lub-dub")
         await asyncio.sleep(10)
 
 
 def main(connector_type):
 
     def_cfg = dict()
-    def_cfg['CONNECTOR_CFG'] = {
-        'connector_address': ('localhost', 9001),
-        'ui_address': ('localhost', 8001)
+    def_cfg["CONNECTOR_CFG"] = {
+        "connector_address": ("localhost", 9001),
+        "ui_address": ("localhost", 8001),
     }
     def_cfg["IFACE_LIST"] = {
-
         "serial_usb0": {
             "INTERFACE": {
                 "MODULE": "daq.interface.interface",
-                "CLASS": "SerialPortInterface"
+                "CLASS": "SerialPortInterface",
             },
             "IFCONFIG": {
                 "LABEL": "serial_usb0",
                 "ADDRESS": "/dev/ttyUSB0",
                 "baudrate": 230400,
-                "SerialNumber": "0001"
-            }
+                "SerialNumber": "0001",
+            },
         }
     }
 
-    if connector_type == 'server':
+    if connector_type == "server":
         con = WSConnectorServer(
             def_cfg,
             # connector_address=('localhost', 9001),
             # ui_address=('localhost', 8001)
         )
-    elif connector_type == 'ui':
+    elif connector_type == "ui":
         con = WSConnectorUI(
             def_cfg,
             # connector_address=('localhost', 9001),
             # ui_address=('localhost', 8001)
         )
     con.start()
-    
+
     event_loop = asyncio.get_event_loop()
     asyncio.ensure_future(heartbeat())
     task_list = asyncio.all_tasks()
@@ -54,7 +53,7 @@ def main(connector_type):
         event_loop.run_until_complete(asyncio.wait(task_list))
         event_loop.run_forever()
     except KeyboardInterrupt:
-        print('closing server')
+        print("closing server")
         # print(f'task_list: {task_list}')
         for task in task_list:
             # print("cancel task")
@@ -65,7 +64,7 @@ def main(connector_type):
 
     finally:
 
-        print('closing event loop')
+        print("closing event loop")
         event_loop.close()
 
 
