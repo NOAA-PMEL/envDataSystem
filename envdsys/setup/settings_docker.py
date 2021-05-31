bc1118b891e51f565be3029d38d7188e327c27a4
Derek Coffman <derek.coffman@noaa.gov>
2021-05-31 08:38:28 -0700

    fix docker build files

--- envdsys/setup/settings_docker.py
+++ envdsys/setup/settings_docker.py
@@ -172,6 +172,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    # 'path_to_static_directory/static/',
+]
 
 # STATIC_URL = '/static/'
 STATIC_URL = '/staticfiles/'
