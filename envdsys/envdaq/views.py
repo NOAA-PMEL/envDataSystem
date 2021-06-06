d1d48a0e5599a827922e35571b7d79113702a810
Derek Coffman <derek.coffman@noaa.gov>
2021-06-06 16:16:25 -0700

    remove errant import

--- envdsys/envdaq/views.py
+++ envdsys/envdaq/views.py
@@ -1,6 +1,5 @@
 from django.http.response import Http404
 from django.shortcuts import get_object_or_404, render
-from matplotlib.pyplot import plot
 
 from envtags.models import Configuration
 from .models import DAQController, DAQInstrument, InstrumentAlias, Controller
